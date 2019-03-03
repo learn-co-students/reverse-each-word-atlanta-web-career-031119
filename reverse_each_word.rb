abe587693064b9cdf59bec12717d0b562068421d
Travis Martin <travisemerymartin@gmail.com>
2019-03-03 02:03:58 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,16 @@
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   reverse_array = []
+#   array.each do |word|
+#     reverse_array.push(word.reverse)
+#   end
+#   reverse_string = reverse_array.join(" ")
+#   return reverse_string
+# end
+
 def reverse_each_word(string)
   array = string.split(" ")
-  reverse_array = []
-  array.each do |word|
-    reverse_array.push(word.reverse)
-  end
+  reverse_array = array.collect {|word| word.reverse}
   reverse_string = reverse_array.join(" ")
   return reverse_string
 end
